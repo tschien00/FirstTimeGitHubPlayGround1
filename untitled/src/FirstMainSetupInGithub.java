3da7e34296f1a8358d0e4e747cb550d9d7b731c9
trenton <schientrenton@ymail.com>
2017-05-31 22:05:09 -0500

    This is a test of how to use github

---
+++ untitled/src/FirstMainSetupInGithub.java
@@ -0,0 +1,8 @@
+/**
+ * Created by trenton on 5/31/2017.
+ */
+public class FirstMainSetupInGithub {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
